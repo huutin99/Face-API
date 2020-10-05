c7f94b90dd8dabc3bd3890ea9b5b15d4713dff55
Tin <maihuynhhuutin@gmail.com>
2020-10-05 21:45:06 +0700

    Fix recognition

--- AppFaceAPI/routes/recognition.js
+++ AppFaceAPI/routes/recognition.js
@@ -1,24 +1,84 @@
-const { detectAllFaces } = require('face-api.js')
+require('@tensorflow/tfjs-node')
+const faceapi = require('face-api.js')
+const fetch = require('node-fetch')
 var fs = require('fs')
-// require('../node_modules/@tensorflow/tfjs-node')
+// const { window } = new JSDOM("<!DOCTYPE html>")
+
+const { JSDOM } = require("jsdom")
+global.window = new JSDOM('<!DOCTYPE html><img id="for-detect">').window
+global.document = window.document
+global.HTMLImageElement = window.HTMLImageElement
+const $ = require("jquery")(window)
+
+faceapi.env.monkeyPatch({
+    fetch: fetch,
+    Canvas: window.HTMLCanvasElement,
+    Image: window.HTMLImageElement,
+    createCanvasElement: () => document.createElement('canvas'),
+    createImageElement: () => document.createElement('img')
+})
 
 module.exports = function (app) {
     app.get('/recognition', function (req, res) {
         res.render('face-recognition')
     })
 
-    app.post('/recognition', function (req, res) {
-        var dirPath = 'img/';
-        var dirList = []; //this is going to contain paths
-        fs.readdirSync(dirPath).forEach(folder => {
-            dirList.push(folder)
-        })
+    app.post('/recognition', async function (req, res) {
         let base64String = req.body.img
-        var img = new Image()
-        img.setAttribute('src', base64String)
-        var dectection = detectAllFaces(img, )
+        var img
+        // var img = $(`<img src="${base64String}">`)
+        await faceapi.nets.tinyFaceDetector.loadFromDisk('./public/models/tiny_face_detector_model-weights_manifest.json')
+        await faceapi.nets.faceRecognitionNet.loadFromDisk('./public/models/face_recognition_model-weights_manifest.json')
+        recognizeImg(img)
+        // await recognizeImg(img)
         res.end(JSON.stringify({
             content: 'OK'
         }))
     })
+}
+
+async function recognizeImg(img) {
+    const labeledFaceDescriptors = await loadLabeledImages()
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+    const detections = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceDescriptors()
+    const displaySize = { width: img.width, height: img.height }
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+    results.forEach(result => {
+        console.log(result.toString())
+    })
+}
+
+function loadLabeledImages() {
+    var dirList = []; //this is going to contain paths
+    fs.readdirSync('img/').forEach(folder => {
+        dirList.push(folder)
+    })
+    return Promise.all(
+        dirList.map(async label => {
+            var descriptions = []
+            for (let i = 1; i <= 2; i++) {
+                var bitmap = fs.readFileSync(`img/${label}/${i}.jpg`)
+                var b64String = Buffer.from(bitmap).toString('base64')
+                // var img = document.getElementById('for-detect')
+                var img = document.createElement('img')
+                img.setAttribute('src', `data:image/jpg;base64,` + b64String)
+
+
+                //-------------------Loi cho nay ne-------------------//
+                // var img = await $(`<img src="data:image/jpg;base64,${b64String}">`)
+                // var img = fetchImage(`http://localhost:9000/images/${label}/${i}.png`);
+                const detection = await faceapi
+                    .detectSingleFace(img, new faceapi.TinyFaceDetectorOptions({ scoreThreshold: 0.3 }))
+                    .withFaceLandmarks()
+                    .withFaceDescriptor();
+                //----------------------------------------------------//
+
+                
+                console.log(img instanceof HTMLImageElement)
+                descriptions.push(detection.descriptor)
+            }
+            return new faceapi.LabeledFaceDescriptors(label, descriptions)
+        })
+    )
 }
