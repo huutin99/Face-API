dfab758d59a444127dc50d9a0a6cc6fd11d538c5
Tin <maihuynhhuutin@gmail.com>
2020-10-08 00:46:12 +0700

    Some mirror fixes

--- routes/recognition.js
+++ routes/recognition.js
@@ -1,6 +1,5 @@
 require('@tensorflow/tfjs-node')
 const faceapi = require('face-api.js')
-const fetch = require('node-fetch')
 var fs = require('fs')
 const canvas = require('canvas')
 const { TinyFaceDetectorOptions } = require('face-api.js')
@@ -17,22 +16,12 @@ module.exports = function (app) {
         res.render('face-recognition')
     })
 
-    app.post('/recognition', async function (req, res) {
-        var dirList = []
-        fs.readdirSync('public/images/').forEach(folder => {
-            var length = fs.readdirSync(`public/images/${folder}/`).length
-            folder += ';' + length
-            dirList.push(folder)
-        })
 
         var base64Data = req.body.img.replace(/^data:image\/png;base64,/, "");
-
         fs.writeFileSync('queryimg/1.jpg', base64Data, 'base64', function (err) {
             console.log(err);
         });
-
         var result = recognizeImg()
-
         res.end(JSON.stringify({
             result: (await result).toString()
         }))
@@ -43,7 +32,6 @@ async function recognizeImg() {
     await faceapi.nets.tinyFaceDetector.loadFromDisk('./public/models')
     await faceapi.nets.faceLandmark68Net.loadFromDisk('./public/models')
     await faceapi.nets.faceRecognitionNet.loadFromDisk('./public/models')
-
     const labeledFaceDescriptors = await loadLabeledImages()
     const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors)
     const queryImg = await canvas.loadImage('./queryimg/1.jpg')
